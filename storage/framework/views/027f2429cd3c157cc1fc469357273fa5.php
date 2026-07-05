571b40410b7055cc8dd31a4d2b565ee11f0a9804
junaidwaqas19 <junaidwaqas509@gmail.com>
2026-07-05 07:04:15 +0500

    Lecture-13-Database-Connections-With-Laravel-Project

---
+++ storage/framework/views/027f2429cd3c157cc1fc469357273fa5.php
@@ -0,0 +1,23 @@
+<?php $__env->startSection('title','User List'); ?>
+<?php $__env->startSection('content'); ?>
+
+<div class="bg-white rounded shadow">
+
+    <div class="flex justify-between p-4 border-b">
+        <h2 class="font-bold">User List</h2>
+
+        <a href="<?php echo e(route('user-add')); ?>" class="px-3 py-2 text-white bg-blue-600 rounded">
+            Add User
+        </a>
+    </div>
+     <div>
+         <li><?php echo e(session('username')); ?></li>
+         <li><?php echo e(session('email')); ?></li>
+     </div>
+
+
+ </div>
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.adminLayout', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\Users\Administrator\Desktop\New folder\laravelTutorial\resources\views/adminPanel/user/userList.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/ee55b4b2969da1353de4aa01fd7acab8.php
@@ -0,0 +1,32 @@
+<div class="w-64 bg-gray-900 text-white hidden md:flex flex-col">
+
+    <div class="p-5 text-2xl font-bold border-b border-gray-700">
+        Admin Panel
+    </div>
+
+    <nav class="flex-1 p-4 space-y-2">
+
+        <a href="#" class="block px-4 py-2 rounded bg-gray-800 hover:bg-gray-700">
+            Dashboard
+        </a>
+
+        <a href="<?php echo e(route('user.list')); ?>" class="block px-4 py-2 rounded hover:bg-gray-700">
+            Users
+        </a>
+
+        <a href="#" class="block px-4 py-2 rounded hover:bg-gray-700">
+            Product
+        </a>
+
+        <a href="#" class="block px-4 py-2 rounded hover:bg-gray-700">
+            Orders
+        </a>
+
+        <a href="#" class="block px-4 py-2 rounded hover:bg-gray-700">
+            Settings
+        </a>
+
+    </nav>
+
+</div>
+<?php /**PATH C:\Users\Administrator\Desktop\New folder\laravelTutorial\resources\views/partials/adminSidebar.blade.php ENDPATH**/ ?>
